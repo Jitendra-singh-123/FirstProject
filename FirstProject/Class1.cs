51f8b1fa7bcf9ba5e35cef4422dc3e3f27708ede
myusername <www.vickysingh31561@gmail.com>
2024-01-11 16:24:56 +0530

    changing the salary in class1.cs

--- FirstProject/Class1.cs
+++ FirstProject/Class1.cs
@@ -12,7 +12,7 @@ namespace FirstProject
             Console.WriteLine("Second class");
             string name = "Jay";
             int roll_no = 21;
-            double salary = 20000;
+            double salary = 21000;
             Console.WriteLine($"name={name} \n roll no = {roll_no} \n salary = {salary}");
             Console.WriteLine("\n\nname={0} \n roll no = {1} \n salary = {2}", name, roll_no, salary);
             Console.WriteLine("\n\nname = " + name + " " + "roll no = " + roll_no +" " + "salary = " + salary);

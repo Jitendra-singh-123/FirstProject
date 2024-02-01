67e71776982560b8bd777d10a671d3a6a4d2f0f9
Jitendra-singh-123 <vickysingh31561@gmail.com>
2024-02-01 12:37:44 +0530

    singleton

---
+++ FirstProject/SingletonDesingPattern.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public sealed class Singleton
+    {
+        private static int numberOfObjects = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            numberOfObjects++;
+            Console.WriteLine("Counter on Increasing: " + numberOfObjects);
+
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine("Number of Objects: "+ numberOfObjects);
+        }
+
+
+    }
+    class SingletonDesingPattern
+    {
+
+        static void Main()
+        {
+            Singleton singleton = Singleton.GetInstance;
+            Singleton singleton1 = Singleton.GetInstance;
+
+            singleton.PrintDetails();
+            singleton1.PrintDetails();
+
+            if(singleton1 == singleton)
+                Console.WriteLine("Both objects are same");
+            else
+            {
+                Console.WriteLine("Both objects are different");
+            }
+            Console.ReadLine();
+        }
+    }
+}
